42c61b7174d65488e7ee6d1141c2c853b976b67c
farleybsd <farley.t.i@hotmail.com>
2020-08-05 13:01:19 -0300

    Utilizando o PHP

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<?php
+$artigos= [
+    [
+    'titulo' => 'Primeiros passos com Spring',
+    'Conteudo'=> ' Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
+    programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
+    'Link' => 'primeiros-passos-com-spring.html'
+    ],
+    [
+        'titulo' => 'O que é Metodologia Ágil?',
+        'Conteudo'=> ' Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
+        'Link' => 'como-e-o-funil-do-growth-hacking.html'
+    ],
+    [
+        'titulo' => ' Como é o funil do Growth Hacking?',
+        'Conteudo'=> ' Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
+        mais sobre ele.',
+        'Link' => 'como-e-o-funil-do-growth-hacking.html'
+    ],
+
+
+    ];
+?>
+
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach($artigos as $artigo) :?>
+        <h2>
+            <a href="<?php echo $artigo['Link']; ?>">
+                <?php echo $artigo['titulo'] ;?>
+            </a>
+        </h2>
+        <p>
+            <?php echo $artigo['Conteudo'];?>
+        </p>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
