08dfbb6a7e7cf1fd7350dff02041fc5c52627565
farleybsd <farley.t.i@hotmail.com>
2020-08-05 13:24:09 -0300

    Refatorando o código

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <!DOCTYPE html>
 <?php
-$artigos= [
-    [
-    'titulo' => 'Primeiros passos com Spring',
-    'Conteudo'=> ' Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-    programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-    'Link' => 'primeiros-passos-com-spring.html'
-    ],
-    [
-        'titulo' => 'O que é Metodologia Ágil?',
-        'Conteudo'=> ' Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
-        'Link' => 'como-e-o-funil-do-growth-hacking.html'
-    ],
-    [
-        'titulo' => ' Como é o funil do Growth Hacking?',
-        'Conteudo'=> ' Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-        mais sobre ele.',
-        'Link' => 'como-e-o-funil-do-growth-hacking.html'
-    ],
-
-
-    ];
+include 'Artigo.php';
+$artigo = new Artigo();
+$artigos = $artigo->exibirTodos();
 ?>
 
 <html lang="pt-br">
