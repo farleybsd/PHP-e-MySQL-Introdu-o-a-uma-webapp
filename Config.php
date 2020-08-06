5ef56586bb4f47b26e0ce039e47ec240165dd207
farleybsd <farley.t.i@hotmail.com>
2020-08-06 07:44:57 -0300

    CORRECAO ERROS AMBIENTE

--- Config.php
+++ Config.php
@@ -1,5 +1,6 @@
 <?php
-$mysql = new mysqli('localhost', 'root', '', 'blog');
+//$mysql = new mysqli('localhost', 'root', '', 'blog'); univale
+$mysql = new mysqli('localhost', 'root', '', 'blog',3307); //casa
 $mysql->set_charset('utf8');
 
 if ($mysql == true) {
