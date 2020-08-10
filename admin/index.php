a0c3011a3a64aa4d27ea20b7a11ae3db63943a3f
farleybsd <farley.t.i@hotmail.com>
2020-08-10 10:23:52 -0300

    Formulário de exclusão de artigos

--- admin/index.php
+++ admin/index.php
@@ -21,8 +21,8 @@ $artigos = $artigo->exibirTodos();
             <div id="artigo-admin">
                 <p><?php echo $art['titulo']; ?></p>
                 <nav>
-                    <a class="botao" href="admin/editar-artigo.html?id=<?php echo $art['id']; ?>">Editar</a>
-                    <a class="botao" href="admin/excluir-artigo.html?id=<?php echo $art['id']; ?>">Excluir</a>
+                    <a class="botao" href="editar-artigo.php?id=<?php echo $art['id']; ?>">Editar</a>
+                    <a class="botao" href="excluir-artigo.php?id=<?php echo $art['id']; ?>">Excluir</a>
                 </nav>
             </div>
           <?php } ?>
